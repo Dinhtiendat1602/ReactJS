655a1092df2b4ca514ca7e4b519b2aa139f90224
Đinh Tiến Đạt <dinhtiendat1602@gmail.com>
2026-07-04 22:23:19 +0700

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://dinhtiendat1602.github.io/ReactJS/',
+  base: '/ReactJS/',
   plugins: [react()],
 })
